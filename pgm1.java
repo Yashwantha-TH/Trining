186cb1e38b4fb5e8fcb43efb4629fc4bc01ba74a
Yashwaantha T H <yashavanthyash102@gmail.com>
2025-07-02 09:28:55 +0530

    Add files via upload

---
+++ pgm1.java
@@ -0,0 +1,13 @@
+public class pgm1 {
+    public static void main(String[] args) {
+        int n=90283567;
+        int s=n%9;
+
+        if(s==0){
+            System.out.println("9");
+        }
+        else{
+            System.err.println(s);
+        }
+    }   
+}
